2f1a60e246f3b750dd69f062e7e336fcd1705d09
Azbakan <154515183+Azbakan@users.noreply.github.com>
2024-01-09 20:59:30 +0300

    Add files via upload

---
+++ faktoriyel hesaplama/faktoriyel hesaplama/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faktoriyel_hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Bir Sayi Giriniz : ");
+            double sayi = Convert.ToDouble(Console.ReadLine());
+            double faktoriyel = 1;
+
+            for (int i = 1; i <=sayi; i++)
+            {
+                faktoriyel = faktoriyel *= i;
+            }
+            Console.WriteLine(sayi + "! = " + faktoriyel);
+            Console.ReadKey();
+        }
+    }
+}
